0fc6e613ef579533bb2787bd292485156dea8a77
benvahaba <51039453+benvahaba@users.noreply.github.com>
2021-07-15 04:10:45 +0300

    first commit

---
+++ FourCs/src/model/LocalStorageManipulation.java
@@ -0,0 +1,125 @@
+package model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class LocalStorageManipulation {
+	private final String localFileName = "ImagesFoldierPath.txt";
+	private File localFile;
+
+	private String imagesFolderPath="";
+	public  String getImagesFolderPath() throws FileNotFoundException 
+	{
+		try {
+			localStorageScanner = new Scanner(localFile);
+		} catch (FileNotFoundException e) {
+			throw e;
+		}
+		if(localStorageScanner.hasNext())
+		{
+			imagesFolderPath=localStorageScanner.nextLine();
+			return imagesFolderPath;
+			
+		}
+		
+		
+		return imagesFolderPath;
+	}
+	private Scanner localStorageScanner = null;
+
+	public LocalStorageManipulation() throws IOException, FileNotFoundException{
+
+		localFile = new File(localFileName);
+
+		
+	
+			try {
+				if (localFile.createNewFile() == false)
+				{
+					localStorageScanner = new Scanner(localFile);
+					if (localStorageScanner.hasNextLine()) {
+						imagesFolderPath=localStorageScanner.nextLine();
+						
+					}				
+				}
+			} catch (FileNotFoundException e) {
+				throw e;
+			} catch (IOException e) {
+				throw e;
+			}
+
+
+		finally
+		{
+			if(localStorageScanner!=null)
+			{
+				localStorageScanner.close();
+			}
+		
+			
+		}
+
+	}
+
+	public Boolean checkIfImagesLocalPathExists() {
+		Boolean PathExists = false;
+		
+		if(!imagesFolderPath.isEmpty())
+		{
+			PathExists=true;
+		}
+
+		return PathExists;
+		//
+
+	}
+	public Boolean InsertNewImagesFolderPath (String i_path) throws IOException 
+	{
+		
+		Boolean pathSavedBoolean=true;
+		FileWriter fileWriter = null;
+		Path imagesDirPath;
+		
+		try {
+			 imagesDirPath =Paths.get(i_path);
+			
+			
+		} catch (InvalidPathException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			pathSavedBoolean=false;
+			return pathSavedBoolean;
+		}
+		
+		try {
+			fileWriter= new FileWriter(localFile);
+			fileWriter.write(imagesDirPath.toString());
+
+		
+		
+		
+		} catch (IOException e) {
+			
+			throw e;
+		}
+		finally {
+			fileWriter.flush();
+			fileWriter.close();
+			
+		}
+		
+		
+		
+		
+		return pathSavedBoolean;
+
+		
+	}
+
+}
---
+++ FourCs/src/tests/storageManipulationTest.java
@@ -0,0 +1,62 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.LocalStorageManipulation;
+
+class storageManipulationTest {
+
+	LocalStorageManipulation localStorageManipulation;
+
+
+
+
+	@Test
+	void imageLocalPathBefore() {
+		
+		LocalStorageManipulation localStorageManipulation = null;
+		try {
+			 localStorageManipulation = new LocalStorageManipulation();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(localStorageManipulation.checkIfImagesLocalPathExists())
+			System.out.println("local path exitst");
+		else {
+			System.out.println("local path DONT exitst\"");
+		}
+		try {
+			System.out.println("image path: "+localStorageManipulation.getImagesFolderPath());
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			localStorageManipulation.InsertNewImagesFolderPath("C:\\Users\\vahab\\Desktop\\android2final");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		localStorageManipulation.checkIfImagesLocalPathExists();
+		try {
+			System.out.println("image path: "+localStorageManipulation.getImagesFolderPath());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
